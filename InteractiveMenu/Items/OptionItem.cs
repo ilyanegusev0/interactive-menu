8596b6988024b487b04c2aea3ef20d5841024fdc
ilyanegusev0 <ilyanegusev0912@gmail.com>
2025-11-17 10:50:38 +0200

    chore(core): add initial project

---
+++ InteractiveMenu/Items/OptionItem.cs
@@ -0,0 +1,25 @@
+﻿namespace InteractiveMenu.Items
+{
+    public sealed class OptionItem : MenuItem
+    {
+        // PROPERTIES:
+
+        public object? Value { get; }
+
+        // CONSTRUCTORS:
+
+        public OptionItem(string text, object? value, ConsoleColor color)
+            : base(text, color, true)
+        {
+            Value = value;
+        }
+
+        public OptionItem(string text)
+            : this(text, null, MenuConfiguration.Configuration.DefaultColor) { }
+        public OptionItem(string text, ConsoleColor color)
+            : this(text, null, color) { }
+        public OptionItem(string text, object value)
+            : this(text, value, MenuConfiguration.Configuration.DefaultColor) { }
+
+    }
+}
---
+++ InteractiveMenu/MenuController.cs
@@ -0,0 +1,92 @@
+﻿using InteractiveMenu.Items;
+
+namespace InteractiveMenu
+{
+    internal sealed class MenuController
+    {
+        // FIELDS:
+
+        private readonly MenuConfiguration _configuration;
+        private readonly MenuRenderer _renderer;
+
+        // CONSTRUCTORS:
+
+        public MenuController(MenuConfiguration configuration, MenuRenderer renderer)
+        {
+            _configuration = configuration;
+            _renderer = renderer;
+        }
+
+        // METHODS:
+
+        // public
+
+        public object? Run(List<MenuItem> items)
+        {
+            Console.CursorVisible = false;
+
+            int selectedIndex = items.FindIndex(IsSelectable);
+            int startRow = Console.GetCursorPosition().Top;
+
+            while (true)
+            {
+                _renderer.Draw(items, selectedIndex, startRow);
+
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedIndex = Navigate(items, selectedIndex, -1);
+
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        selectedIndex = Navigate(items, selectedIndex, 1);
+
+                        break;
+
+                    case ConsoleKey.Enter:
+                        Console.CursorVisible = true;
+
+                        var selectedItem = items[selectedIndex] as OptionItem;
+                        if (selectedItem != null)
+                        {
+                            if (selectedItem.Value != null)
+                                return selectedItem.Value;
+
+                            int logicalIndex = items.OfType<OptionItem>().ToList().IndexOf(selectedItem);
+
+                            return logicalIndex;
+                        }
+
+                        break;
+
+                    case ConsoleKey.Escape:
+                        Console.CursorVisible = true;
+                        return null;
+                }
+            }
+        }
+
+        // private 
+
+        private static bool IsSelectable(MenuItem item) => item.IsSelectable;
+
+        private static int Navigate(List<MenuItem> items, int currentIndex, int direction)
+        {
+            int newIndex = currentIndex;
+
+            do
+            {
+                newIndex = (newIndex + direction + items.Count) % items.Count;
+
+                if (newIndex == currentIndex)
+                    break;
+            }
+            while (!IsSelectable(items[newIndex]));
+
+            return newIndex;
+        }
+    }
+}
